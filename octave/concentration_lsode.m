1e2c6336d6929f33038b7ff69365b83faa7bee3e
s3e3 <48084258+siatos@users.noreply.github.com>
2022-03-25 20:14:05 +0200

    minor modifications

--- octave/concentration_lsode.m
+++ octave/concentration_lsode.m
@@ -5,58 +5,45 @@ initial_values = [0, 0];
 times = 0:0.02:3;
 
 function dsdt = concetration1(s,t) 
-
-	% Constant parameters of the ODE
-	
-  % Provided quantities
-  k1 = 20;		
-  k2 = 5;		
-  k3 = 5;		
-  k4 = 5;		
-  k5 = 2;		
-  K = 1;		
-  n = 4
-
-
-	dsdt(1) = k1/(1+power((s(2)/K), n))-k3*s(1)-k5*s(1);
-	dsdt(2) = k2 + k5*s(1)-k4*s(2)
-
+   % Constant parameters of the ODE
+   % Provided quantities
+   k1 = 20;		
+   k2 = 5;		
+   k3 = 5;		
+   k4 = 5;		
+   k5 = 2;		
+   K = 1;		
+   n = 4
+   dsdt(1) = k1/(1+power((s(2)/K), n))-k3*s(1)-k5*s(1);
+   dsdt(2) = k2 + k5*s(1)-k4*s(2)
 endfunction
 
 function dsdt = concetration2(s,t) 
-
-	% Constant parameters of the ODE
-	
-  % Provided quantities
-  k1 = 30;		
-  k2 = 6;		
-  k3 = 6;		
-  k4 = 6;		
-  k5 = 3;		
-  K = 2;		
-  n = 5
-
-	dsdt(1) = k1/(1+power((s(2)/K), n))-k3*s(1)-k5*s(1);
-	dsdt(2) = k2 + k5*s(1)-k4*s(2)
-
+   % Constant parameters of the ODE
+   % Provided quantities
+   k1 = 30;		
+   k2 = 6;		
+   k3 = 6;		
+   k4 = 6;		
+   k5 = 3;		
+   K = 2;		
+   n = 5
+   dsdt(1) = k1/(1+power((s(2)/K), n))-k3*s(1)-k5*s(1);
+   dsdt(2) = k2 + k5*s(1)-k4*s(2)
 endfunction
 
 function dsdt = concetration3(s,t) 
-
-	% Constant parameters of the ODE
-	
-  % Provided quantities
-  k1 = 10;		
-  k2 = 2;		
-  k3 = 2;		
-  k4 = 2;		
-  k5 = 2;		
-  K = 1;		
-  n = 3
-
-	dsdt(1) = k1/(1+power((s(2)/K), n))-k3*s(1)-k5*s(1);
-	dsdt(2) = k2 + k5*s(1)-k4*s(2)
-
+   % Constant parameters of the ODE
+   % Provided quantities
+   k1 = 10;		
+   k2 = 2;		
+   k3 = 2;		
+   k4 = 2;		
+   k5 = 2;		
+   K = 1;		
+   n = 3
+   dsdt(1) = k1/(1+power((s(2)/K), n))-k3*s(1)-k5*s(1);
+   dsdt(2) = k2 + k5*s(1)-k4*s(2)
 endfunction
 
 
