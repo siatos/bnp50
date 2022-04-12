f8ceff8f09724823ac033966fe7bf23ddf50bd04
s3e3 <48084258+siatos@users.noreply.github.com>
2022-04-12 18:58:57 +0000

    correction

--- octave/membrane_potential.m
+++ octave/membrane_potential.m
@@ -1,100 +1,71 @@
 clear all; 
 clf;
 
-% Time interval
-%Tinterval = linspace(0, 10, 1001)   
-%  
-
+%% Function definition %%%
 function res = membrane_potential(t, p)
    % p is 1x4 vector for the parameters of the equation
    E  = p(1)  % E = Nernst potential
    V0 = p(2)  % V0 = Potential at t = 0 
    G  = p(3)  % G = conductance of the membrane
-   C  = p(4)  % C = capacitance 
-   
+   C  = p(4)  % C = capacitance    
    res =  E - (E - V0)*exp(-(G/C)*t) 
 end;
 
 
-% declare a cell of 1x3 strings to be used as legend initial value  %
-lgnd = {"", "", ""}
+% declare a cell of 1x5 strings to be used as legend initial value  %
+% since we will have 5 different cases
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% FIRST CASE - ALL PARAMS SAME  EXCEPT V0 POTENTIAL: THREE CASES % 
-% V0 < E                                                         %
-% V0 = E                                                         % 
-% V0 > E                                                         %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+lgnd = {"", "", "", "",""}
 
-%Toggle - set the "hold" state of the plotting engine
-%hold on
-figure(1)  % 1st case all parameters same except V0
 
-%%%  Parameter vector  - 3 sets of params        %%
-params1 = [-75 -100 0.02 0.0022; -75 -50 0.04 0.0022;  -75 -75 0.01 0.0010]
+%%%%%%% Parameter vector  - 5 sets of params %%%%%%%%%%%%
+%%                                                     %%
+%%    First three cases = relationship between e, V0   %%
+%%    The other two concentrate on the ratio g/C       %%  
+%%                                                     %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
+params = [-75 -50 0.02 0.04;  
+           -75 -75 0.02 0.04;
+           -75 -100 0.01 0.04;
+           -75 -100 0.04 0.02;
+           -75 -100 4.2 0.4]
 
 % loop through the rows of params vector and calculate function values %
-for i = 1:3 % we have 3 input vectors  
-    for j = 1:101 % time values from 0.0..1 with step 0.01 
+for i = 1:5 % we have 5 input parameter sets  
+    for j = 1:2001 % time values from 0.0..1 with step 0.01 
         t(i,j) = (j-1)/100;
-        funcg (i, j) =  membrane_potential (t(i,j), params1(i,:));
+        funcg(i, j) =  membrane_potential (t(i,j), params(i,:));
     end
     % create also a legend for each graph
-    lgnd{i} = sprintf("Ε= %2.1f V0= %2.1f\n", params1(i, 1), params1(i, 2))
+    lgnd{i} = sprintf("Ε= %2.1f V0= %2.1f G/C= %2.4f \n", params(i, 1), params(i, 2), params(i, 3)/params(i, 4))
 end
 
+
 % plot the graphs
-plot(t(1,:), funcg(1,:),' r', 'linewidth', 2.5, t(2,:), funcg(2,:),' k', 'linewidth', 2.5, t(3,:), funcg(3,:),' c', 'linewidth', 2.5)
+plot(t(1,:), funcg(1,:),' r', 'linewidth', 2.5, 
+     t(2,:), funcg(2,:),' k', 'linewidth', 2.5, 
+     t(3,:), funcg(3,:),' c', 'linewidth', 2.5,
+     t(4,:), funcg(4,:),' g', 'linewidth', 2.5,
+     t(5,:), funcg(5,:),' y', 'linewidth', 2.5)
+     
+
+
 % display specifics %
 xlabel('t')
 ylabel('Membrane Potential (mV)')
+
 % set grid on
 grid on                                   
+
 % additional display properties
 set(gca, 'fontsize', 20, 'linewidth', 1)  
-title('Μεμβρανικά Δυναμικά - Case 1 - Different V0 potential')
+title('Μεμβρανικά Δυναμικά Different parameters')
 
 % add a legend
-legend (lgnd{1}, lgnd{2}, lgnd{3}, "location", "northeast");                
+legend (lgnd{1}, lgnd{2}, lgnd{3}, lgnd{4}, lgnd{5},"location", "northeast");                
 set (legend, "fontsize", 12);
-% release hold
-%hold off
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SECOND CASE - DIFFERENT PARAMS                                 % 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Toggle - set the "hold" state of the plotting engine
-%hold on
-figure(2)
 
-%%%  Parameter vector  - 3 sets of params        %%
-params2 = [-75 -100 0.02 0.0022; -75 -100 0.04 0.0022;  -75 -100 0.04 0.022]
-
-% loop through the rows of params vector and calculate function values %
-for i = 1:3 % we have 3 input vectors  
-    for j = 1:301 % time values from 0.0..3 with step 0.01 
-        t(i,j) = (j-1)/100;
-        funcg (i, j) =  membrane_potential (t(i,j), params2(i,:));
-    end
-    % create also a legend for each graph
-    lgnd{i} = sprintf("Ε= %2.1f V0= %2.1f G/C= %2.4f \n", params2(i, 1), params2(i, 2), params2(i, 3)/params2(i, 4))
-end
-
-% plot the graphs
-plot(t(1,:), funcg(1,:),' r', 'linewidth', 2, t(2,:), funcg(2,:),' k', 'linewidth', 2, t(3,:), funcg(3,:),' c', 'linewidth', 2)
-% display specifics %
-xlabel('t')
-ylabel('Membrane Potential (mV)')
-% set grid on
-grid on                                   
-% additional display properties
-set(gca, 'fontsize', 20, 'linewidth', 1)  
-title('Μεμβρανικά Δυναμικά - Case 2 - Different params')
-% add a legend
-legend (lgnd{1}, lgnd{2}, lgnd{3}, "location", "northeast");                
-set (legend, "fontsize", 12);
-% release hold
-%hold off
 
 
