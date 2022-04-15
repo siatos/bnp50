6ffd11ac19cac60bcf0beeb4d44ba3438a3eda9a
s3e3 <48084258+siatos@users.noreply.github.com>
2022-04-15 17:45:58 +0300

    minor update

--- octave/membrane_potential.m
+++ octave/membrane_potential.m
@@ -38,7 +38,7 @@ for i = 1:5 % we have 5 input parameter sets
         funcg(i, j) =  membrane_potential (t(i,j), params(i,:));
     end
     % create also a legend for each graph
-    lgnd{i} = sprintf("Ε= %2.1f V0= %2.1f G/C= %2.4f \n", params(i, 1), params(i, 2), params(i, 3)/params(i, 4))
+    lgnd{i} = sprintf("Γ…= %2.1f V0= %2.1f G/C= %2.4f \n", params(i, 1), params(i, 2), params(i, 3)/params(i, 4))
 end
 
 
@@ -60,7 +60,7 @@ grid on
 
 % additional display properties
 set(gca, 'fontsize', 20, 'linewidth', 1)  
-title('Μεμβρανικά Δυναμικά Different parameters')
+title('ΞΞµΞΌΞ²ΟΞ±Ξ½ΞΉΞΊΞ¬ Ξ”Ο…Ξ½Ξ±ΞΌΞΉΞΊΞ¬ -Different parameters')
 
 % add a legend
 legend (lgnd{1}, lgnd{2}, lgnd{3}, lgnd{4}, lgnd{5},"location", "northeast");                
