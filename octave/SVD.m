50b5cd84d4352829b2f7be900eb1a319bfc9fb62
s3e3 <48084258+siatos@users.noreply.github.com>
2022-04-26 19:47:58 +0300

    Update SVD.m

--- octave/SVD.m
+++ octave/SVD.m
@@ -32,7 +32,7 @@ M = magic(5)
 [U, S, V] = svd(M)
 % print initial matrix M row by row %
              
-fprintf(fh, '***** results for the magic matrix ******\n')
+fprintf(fh, '***** results for the magic matrix M ******\n')
 fprintf(fh, 'Initial magic M:\n');
 fprintf(fh, '%f %f %f %f %f\n', M(1,:));
 fprintf(fh, '%f %f %f %f %f\n', M(2,:));
@@ -48,5 +48,5 @@ fprintf(fh, '%f %f %f %f %f\n', S(3,:));
 fprintf(fh, '%f %f %f %f %f\n', S(4,:));
 fprintf(fh, '%f %f %f %f %f\n', S(5,:));
 fprintf(fh, '\n')
-fprintf(fh, '*****************************************\n')
-fclose(fh);
+fprintf(fh, '*******************************************\n')
+fclose(fh);  % close file %
