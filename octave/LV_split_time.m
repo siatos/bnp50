ecc6f97e6e4cd2b96b5ac40d8ff669e97c8afc93
s3e3 <48084258+siatos@users.noreply.github.com>
2022-04-26 18:21:07 +0300

    Update LV_split_time.m

--- octave/LV_split_time.m
+++ octave/LV_split_time.m
@@ -4,19 +4,15 @@
 y01(1) = 20  % Initial predator population
 y01(2) = 80  % Initial prey population
  
-%y0 = [20, 80];          % initial values of x & y
-%Tinterval = 0:0.05:50   % time step of 0.01 in total 201 values
-
-% Time interval
-Tinterval = linspace(0, 50, 1001)   % time step of 0.05 in total 1001 values
-% instead of using Tinterval we split in two halves: Tinterval=Tinterval1+Tinterval2 
+% Time intervals
+% Tinterval = linspace(0, 50, 1001)   % time step of 0.05 in total 1001 values
+% but instead of using Tinterval we split in two halves: Tinterval=Tinterval1+Tinterval2 
 Tinterval1 = linspace(0, 24.95, 500)   % time step of 0.05 in total 500 values
-Tinterval2 = linspace(25, 50, 501)   % time step of 0.05 in total 501 values
+Tinterval2 = linspace(25, 50, 501)     % time step of 0.05 in total 501 values
 
 % we have two set of params declared as a vector of two rows
 % first row will be used for t < 25
 % second row will be used for 25 <= t < 50
-%params = [40 2 0.5 70; 20 1 0.25 35]
 params1 = [40 2 0.5 70];
 params2 = [20 1 0.25 35]
 
@@ -33,7 +29,7 @@ function res = LV(t,V, params)
     % v2 is the prey population at time t 
     v1 = V(1);
     v2 = V(2);
-    % selection of param set depends on time
+    % pass params to α(=a), β(=b), γ(=c), δ(=d)
     a = params(1);
     b = params(2);
     c = params(3); 
@@ -57,7 +53,7 @@ rate2 = @(t,V) LV(t,V, params2);
 [T2,V2] = ode45(rate2, Tinterval2, y01); 
 
 
-figure(1)
+figure(1)   % First figure for the populations
 lgnd1 = sprintf("%s%s", "X(t):", "Θηρευτής t < 25")
 lgnd2 = sprintf("%s%s", "Y(t):", "Θήραμα t < 25")
 lgnd3 = sprintf("%s%s", "X(t):", "Θηρευτής 25<= t <50")
@@ -78,7 +74,7 @@ set(gca, 'fontsize', 24, 'linewidth', 1)
 title('Lotka-Voltera Prey-Predator model')
 
 
-figure(2)
+figure(2) % Second figure for the sum of populations
 lgnd1 = sprintf("%s%s", "X(t)+Y(t):", "Θηρευτής+Θήραμα t < 25")
 lgnd2 = sprintf("%s%s", "X(t)+Υ(t):", "Θηρευτής+Θήραμα 25 < t")
 
@@ -96,7 +92,7 @@ title('Lotka-Voltera Prey-Predator model - Sum of Populations')
 
 
 
-figure(3) % First figure for the phase portrait t < 25 
+figure(3) % Third figure for the phase portrait t < 25 
 % Phase portait is a plot of X(t)-Θηρευτής vs Y(t)-Θήραμα
 h = plot(V1(:,1), V1(:,2), 'k', 'linewidth', 1)   
 % display specifics %
@@ -108,14 +104,14 @@ grid on
 set(gca, 'fontsize', 24, 'linewidth', 1)  
 title('Phase Portrait for Lotka-Voltera Model 1st case t < 25')
 
-figure(4) % Second figure for the phase portrait 25 <= t < 50
+figure(4) % Forth figure for the phase portrait 25 <= t < 50
 % Phase portait is a plot of X(t)-Θηρευτής vs Y(t)-Θήραμα
 h = plot(V2(:,1), V2(:,2), 'k', 'linewidth', 1)   
 % display specifics %
-xlabel('X(t): Θηρευτής 25 < t')
-ylabel('Y(t): Θήραμα 25 < t')
+xlabel('X(t): Θηρευτής 25 <= t')
+ylabel('Y(t): Θήραμα 25 <= t')
 % set grid on
 grid on                                   
 % additional display properties
 set(gca, 'fontsize', 24, 'linewidth', 1)  
-title('Phase Portrait for Lotka-Voltera Model 2nd case t > 25')
+title('Phase Portrait for Lotka-Voltera Model 2nd case t >= 25')
