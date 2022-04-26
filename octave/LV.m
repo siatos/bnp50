4d2bca993d96273a786b840ef9c52e746dd2599b
s3e3 <48084258+siatos@users.noreply.github.com>
2022-04-26 18:19:24 +0300

    Update LV.m

--- octave/LV.m
+++ octave/LV.m
@@ -5,35 +5,31 @@ y01(1) = 20  % Initial predator population
 y01(2) = 80  % Initial prey population
  
 % Time interval
-Tinterval = linspace(0, 50, 1001)   % time step of 0.05 in total 1001 values
+Tinterval = linspace(0, 50, 1001)   % time step of 0.05 in total 1001 values %
 
-% we have two set of params declared as a vector of two rows
-% first row will be used for t < 25
-% second row will be used for 25 <= t < 50
-params = [40 2 0.5 70; 20 1 0.25 35]
-
+% Different colors for predator and prey %
 colors = ['k', 'r']  % predator (X) color black, prey (Y) color red 
 
 %%%%%%%%% Solve system of odes  %%%%%%%%%%%%%%%%%%%%%%%
 % define a function that contains the system of LV odes
-function res = LV(t,V, params)
+function res = LV(t,V)
   
     % Dependent variables V - vector of v1 & v2
     % v1 is the predator population at time t
     % v2 is the prey popul;ation at time t 
     v1 = V(1);
     v2 = V(2);
-    % selection of param set depends on time
+    % set of param set depends on time
     if (t < 25)  % use first row of params 
-      a = params(1, 1);
-      b = params(1, 2);
-      c = params(1, 3); 
-      d = params(1, 4);
+      a = 40;
+      b = 2;
+      c = 0.5; 
+      d = 70;
      else        % use second row of params
-      a = params(2, 1);
-      b = params(2, 2);
-      c = params(2, 3); 
-      d = params(2, 4);
+      a = 20;
+      b = 1;
+      c = 0.25; 
+      d = 35;
      endif 
     dxdt = (-d)*v1+c*v1*v2;   % rate of predator population X
     dydt = a*v2 - b*v1*v2     % rate of prey population Y
@@ -42,12 +38,12 @@ function res = LV(t,V, params)
 end
 
 % use anonymous function and get the handle
-rate2 = @(t,V) LV(t,V, params);
+rate = @(t,V) LV(t,V);
    
 % solve odes by using ode45 and anonymous function handle
 % y01 is the initial value vector
 % return a vector of type (t, v) where v = v(1), v(2) at any time t
-[T,V] = ode45(rate2, Tinterval, y01); 
+[T,V] = ode45(rate, Tinterval, y01); 
 
 %%%%%%%% First figure %%%%%%%%%%%%%%%
 % Both poulations  X(t), Y(t) will be displayed
@@ -71,7 +67,7 @@ title('Lotka-Voltera Prey-Predator model, X & Y populations')
 %%%%%%%%%%% Second figure %%%%%%%%%%%%%%%
 % Only sum of X(t)+Y(t) will be displayed
 figure(2)
-lgnd3 = sprintf("%s", "N(t): Άθροισμα Πληθυσμών X+Y")   
+lgnd3 = sprintf("%s", "N(t): ʼθροισμα Πληθυσμών X+Y")   
 h2 = plot(T, V(:,1)+ V(:,2), 'g', 'linewidth', 1, 'DisplayName', lgnd3)   
 legend({lgnd3});
 % display specifics %
