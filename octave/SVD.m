d6eb115a0b9c723bc1ae929db2ad54e5649d1382
s3e3 <48084258+siatos@users.noreply.github.com>
2022-04-27 17:13:59 +0300

    Update SVD.m

--- octave/SVD.m
+++ octave/SVD.m
@@ -1,37 +1,54 @@
+%%%%% Calculate SVD for a givem matrix %%%%%
+
 % open a file to write output %
 fh = fopen('result.txt','w');
+
+%%%%% Input matrix A to calculate      %%%%%%
 A = [6,4; 4,0]
-% Find SVD of A %
+
+%%%%%% Find SVD of A %%%%%%
 [U, S, V] = svd(A)
-% print initial matrix A row by row %
+
+%%%%% since A is 2x2 all matrices S, U & V will be 2x2 %%%%%%%%%
+
+%%%%% print in the ouput file initial matrix A row by row %%%%%%
 fprintf(fh, '**************** results *****************\n')
 fprintf(fh, 'Initial A:\n');
 fprintf(fh, '%f %f\n', A(1,:));
 fprintf(fh, '%f %f\n', A(2,:));
 fprintf(fh, '\n')
-% print diagonal S row by row %
+
+%%%%% print in the output file diagonal matrix S row by row %%%%%%%
 fprintf(fh, 'SVD diagonal S:\n');
 fprintf(fh, '%f %f\n', S(1,:));
 fprintf(fh, '%f %f\n', S(2,:));
 fprintf(fh, '\n')
-% print U left singular row by row %
+
+%%%%% print in the output file U (left singular matrxi) row by row %%%%%
 fprintf(fh, 'Left Singular U:\n');
 fprintf(fh, '%f %f\n', U(1,:));
 fprintf(fh, '%f %f\n', U(2,:));
 fprintf(fh, '\n')
-% print V right singular row by row %
+
+%%%%% print in the output file V (right singular matrxi) row by row %%%%%fprintf(fh, 'Right Singular V:\n');
 fprintf(fh, 'Right Singular V:\n');
 fprintf(fh, '%f %f\n', V(1,:));
 fprintf(fh, '%f %f\n', V(2,:));
+
 fprintf(fh, '*****************************************\n')
 fprintf(fh, '\n\n')
 
-fprintf('running for a magic 5x5 matrix\n')
+
+fprintf('running for a magic 5x5 matrix...')
+fprintf('a new (magic) matrix M will be created ...\n')
+
 M = magic(5)
-% Find SVD of M %
+
+%%%%% Find SVD of M %%%%%%%
 [U, S, V] = svd(M)
-% print initial matrix M row by row %
-             
+
+%%%%%%% print initial matrix M row by row %%%%%%%%%%
+%%%%% since M is 5x5 all matrices S, U & V will be 5x5 %%%%%%%%%
 fprintf(fh, '***** results for the magic matrix M ******\n')
 fprintf(fh, 'Initial magic M:\n');
 fprintf(fh, '%f %f %f %f %f\n', M(1,:));
@@ -40,7 +57,8 @@ fprintf(fh, '%f %f %f %f %f\n', M(3,:));
 fprintf(fh, '%f %f %f %f %f\n', M(4,:));
 fprintf(fh, '%f %f %f %f %f\n', M(5,:));
 fprintf(fh, '\n')
-% print diagonal S row by row %
+
+%%%%%%% print diagonal S row by row %%%%%
 fprintf(fh, 'SVD diagonal S:\n');
 fprintf(fh, '%f %f %f %f %f\n', S(1,:));
 fprintf(fh, '%f %f %f %f %f\n', S(2,:));
