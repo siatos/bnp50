54a8dbb4db9df55b1cf4ba3a163955179f0f02da
s3e3 <48084258+siatos@users.noreply.github.com>
2022-05-02 21:23:33 +0300

    Update solution - try to use 'kiss' principle

--- octave/CA_Bind_rate.m
+++ octave/CA_Bind_rate.m
@@ -1,31 +1,23 @@
-%Initial values and params
-clear all; clf;
+%clear all
+clear all; 
+clf;
 
-function [Prot2Cax, Prot1Cay] = rate(K1, K2, KI, Prot2T, Prot1T, x)
-    CAx = x;
-    CAy = x; 
-    Prot2Cax = (Prot2T*CAx)/((K2+CAx)*(1+(CAx/KI)));
-    Prot1Cay = (Prot1T*CAy)/(K1+CAy)
-endfunction
-clear;
-
-%
+%Initial values and params %
 K1 = 5.88; 
 K2 = 6.25*10^(-2);
 KI = 2.2*10^(-2);
 Prot2T = 10^(-2);
 Prot1T = 10^(-2);
 
-T=linspace(0,1,101)  %  
-CARate=zeros(101,2)    % 1001x2 matrix
+%100+1 values - we look in the closed [0,1] interval step is 0.01 %
+t=linspace(0,1,101)    
 
-for t = 1:101  % 
-   tslice = (t-1)/100; %time from 0.0..1.0 step 0.01
-   [CARate(t, 1), CARate(t,2)] = rate(K1, K2, KI, Prot2T, Prot1T, tslice);
-end
-plot(T, CARate(:, 1), 'r', 'linewidth', 2, T, CARate(:, 2), 'k', 'linewidth', 2)
-%Y=result(:,2)
-%Z=result(:,1)
-%plot(Y, tslice)
+% functions we need to calculate and plot %
+% t is actulaly a vector - we use scalar multiplication, division there the "dot" beside in every sign %
 
+Prot2Cax = (t .* Prot2T) ./((t .+ K2) .* (1+(t ./ KI)));
+Prot1Cay = (Prot1T .* t) ./ (K1 .+ t)
+
+% plot the functions
+plot(t, Prot2Cax, 'r', 'linewidth', 2, t, Prot1Cay, 'k', 'linewidth', 2)
 
