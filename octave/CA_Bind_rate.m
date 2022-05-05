58d5150acd9464a1574fae1ac0421e55ab233205
s3e3 <48084258+siatos@users.noreply.github.com>
2022-05-05 19:07:14 +0300

    Update CA_Bind_rate.m

--- octave/CA_Bind_rate.m
+++ octave/CA_Bind_rate.m
@@ -10,14 +10,36 @@ Prot2T = 10^(-2);
 Prot1T = 10^(-2);
 
 %100+1 values - we look in the closed [0,1] interval step is 0.01 %
-t=linspace(0,1,101)    
+x=linspace(0,1,101)    
 
-% functions we need to calculate and plot %
-% t is actulaly a vector - we use scalar multiplication, division therefore the "dot" beside in every sign %
+% x will be used for vars CAx, CAy              %
+% calculate and plot concentraion of            %  
+% proteins Prot2Cax & Prot1Cay based on the     %
+% below equations                               %
+%                                               %
+% Note: x is actualy a vector - i.e we use      %
+% scalar multiplication, division etc           %
+% therefore the "dot" beside in every sign      %
 
-Prot2Cax = (t .* Prot2T) ./ ((t .+ K2) .* (1+(t ./ KI)));
-Prot1Cay = (Prot1T .* t) ./ (K1 .+ t)
+% set CAx, CAy to x (we could use x directly)   %
+CAx = x
+CAy = x
+% calculate protein concentrations Prot2Cax & Prot1Cay %
+Prot2Cax = (CAx .* Prot2T) ./ ((CAx .+ K2) .* (1+(CAx ./ KI)));
+Prot1Cay = (Prot1T .* CAy) ./ (K1 .+ CAy)
 
-% plot the functions
-plot(t, Prot2Cax, 'r', 'linewidth', 2, t, Prot1Cay, 'k', 'linewidth', 2)
+% plot the functions Prot2Cax(CAx) & Prot1Cay(CAy), CAx=CAy=x in [0,1]  %
+plot(x, Prot2Cax, 'r', 'linewidth', 2, x, Prot1Cay, 'k', 'linewidth', 2)
 
+xlabel('x')
+ylabel('Prot2Cax & Prot1Cay')
+% set grid on
+grid on                                   
+% additional display properties  for the figure
+set(gca, 'fontsize', 16, 'linewidth', 1)  
+title('Binding curves of the two proteins Prot2Cax & Prot1Cay')
+%  set legends   %
+lgnd{1} = sprintf("Protein: Prot2Cax\n")
+lgnd{2} = sprintf("Protein: Prot1Cay\n")
+legend (lgnd{1}, lgnd{2},"location", "northeast");                
+set (legend, "fontsize", 12);
